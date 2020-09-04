1a4040a5c716d2b43c7e37f0393f544343bb736c
condimentman <70375074+condimentman@users.noreply.github.com>
2020-09-04 21:18:41 +0100

    Update safety.js

--- client/src/dashboard/views/about/safety.js
+++ client/src/dashboard/views/about/safety.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+
+const safetyAbout = [
+    
+];
 
 class SafetyAbout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: false
+        }
+    }
+
     render() {
         return (
-            `safety about`
+            <div className="content">
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card>
+                                <Card.Body>
+                                    <h2>Cambridge University Mountaineering Club Safety Policy</h2>
+                                    <p>{safetyAbout}</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         )
     }
 }
