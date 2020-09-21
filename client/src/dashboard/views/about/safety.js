b5bbe7872b868b01cb69b6512b93d50ad2cc3dd3
condimentman <70375074+condimentman@users.noreply.github.com>
2020-09-21 12:52:52 +0100

    New Documents
    Have stuck marked-up versions of relevant documents in the safety page in a few consts

--- client/src/dashboard/views/about/safety.js
+++ client/src/dashboard/views/about/safety.js
@@ -4,10 +4,325 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 
+const constitutionAbout = [
+<p>
+<h2><strong>CAMBRIDGE UNIVERSITY MOUNTAINEERING CLUB CONSTITUTION</strong></h2>
+</p>,
+<p>
+<h3><strong>1.	NAME:</strong></h3>
+</p>,
+<p>
+The name of the Society shall be “Cambridge University Mountaineering Club”
+hereafter referred to as “the Club”.
+</p>,
+<p>
+<h3><strong>2.	OBJECTS:</strong></h3>
+</p>,
+<p>
+    (1)	The Club’s aims shall be:
+</p>,
+<ul>
+    <li>To promote the interests of climbing and mountaineering amongst members of
+    the Club and University.</li>
+    <li>To provide an opportunity for members of the Club to meet and participate in
+    climbing and mountaineering activities together.</li>
+    <li>To act on behalf of and in the interests of Club Members.</li>
+    <li>To promote awareness of Club Members of the need to maintain access,
+    conservation and protection of the cliff and mountain environment.</li>
+    <li>To promote safe climbing and mountaineering practices by Club Members</li>
+</ul>,
+<p>
+</p>,
+<p>
+<h3><strong>3.	 MEMBERSHIP:</strong></h3>
+</p>,
+<p>
+(1)	Membership of the Club shall be open to all members of the University
+and any others that have an interest in mountaineering or rock climbing.
+</p>,
+<p>
+(2) 	There shall be an annual fee for membership which shall be determined
+by the Executive Committee from time to time.
+</p>,
+<p>
+(3)	The Club may offer Life Membership and Honorary Membership on such terms
+as it may decide;
+</p>,
+<p>
+(4)	The Executive Committee may expel any member whose conduct seems likely
+to bring the Club into disrepute. Such expulsion shall be undertaken in
+accordance with Section 8, and is subject to confirmation at the next General
+Meeting of the Club. Any person expelled shall have the right of appeal to the
+Senior Treasurer.
+</p>,
+<p>
+(5)	Membership of the Club shall be open only to individuals who endorse the
+BMC Participation Statement: “The BMC recognises that climbing, hill walking and
+mountaineering are activities with a danger of personal injury or death.
+Participants in these activities should be aware of and accept these risks and
+be responsible for their own actions and involvement.”
+</p>,
+<p>
+<h3><strong>4. 	THE EXECUTIVE COMMITTEE:</strong></h3>
+</p>,
+<p>
+(1)	The day-to-day management of the Club’s affairs shall be in the hands of
+the Executive Committee, which shall normally be elected at the Annual General
+Meeting (AGM);
+</p>,
+<p>
+(2)	The Executive Committee shall consist of a President, Secretary, Junior
+Treasurer and any number of members without portfolio who shall be elected by
+the members as in 4(1). In addition there shall be a Senior Treasurer, who shall
+be a member of the Regent House, or other person approved by the Junior Proctor.
+The Senior Treasurer shall be appointed by the elected members of the Executive
+Committee and shall be <em>ex officio</em> a member of the Executive Committee;
+</p>,
+<p>
+</p>,
+<p>
+(3)	A majority of the elected members of the Executive Committee shall be
+full-time undergraduate or postgraduate students of the University of Cambridge;
+</p>,
+<p>
+(4)	Meetings of the Executive Committee shall be chaired by the President or
+if in absence the Secretary. If neither the President nor Secretary is present,
+the remaining members shall elect a chairperson for that meeting. The quorum for
+a meeting of the Executive Committee shall be three members and a written record
+of each meeting shall be kept;
+</p>,
+<p>
+<h3><strong>5.	GENERAL MEETINGS:</strong></h3>
+</p>,
+<p>
+(1)	The Club shall hold an Annual General Meeting (AGM) during each
+Academical year. The AGM shall be held in Cambridge during <em>Easter Full
+Term</em>. All Members, other than Honorary Members, shall be entitled to attend
+and vote at any General Meeting. At least fourteen days written notice shall be
+given to members before the AGM;
+</p>,
+<p>
+(2)	The AGM shall approve Minutes of the last General Meeting and the Club’s
+Accounts for the preceding year, elect the Executive Committee for the year
+ahead and conduct such other business as is necessary;
+</p>,
+<p>
+(3) Candidates for election to office shall be proposed and seconded by two
+other members. Every motion at a General Meeting shall be proposed and seconded
+by two members. Voting shall be by secret ballot and if there are more than two
+candidates for a post or more than two options on a motion, voting shall be by
+Single Transferable Vote;
+</p>,
+<p>
+(4) An Extraordinary General Meeting (EGM) may be held at any time during
+Full Term. It shall be held in Cambridge and may be called by the Executive
+Committee or at the written request of at least ten members. Twenty-one days
+written notice shall be given to members before an EGM is held. An EGM shall
+have the same powers as an AGM;
+</p>,
+<p>
+(5)	The President or if in absence the Secretary shall take the Chair at any
+General Meeting. In the absence of the President and Secretary the meeting shall
+elect a Chairperson for that meeting. The quorum for a General Meeting shall be
+five members and a written record of every General Meeting shall be kept.
+</p>,
+<p>
+<h3><strong>6.	FINANCIAL MATTERS:</strong></h3>
+</p>,
+<p>
+(1)	The Club shall maintain a banking account with a suitable Bank or
+Building Society to hold the Club’s funds;
+</p>,
+<p>
+(2)	It shall be the responsibility of the Junior Treasurer to ensure that
+monies received are properly accounted for, and that the Club’s financial
+records are kept in good order. In particular, the Junior Treasurer shall ensure
+continuity of Cambridge resident signatories for any bank accounts held by the
+Club;
+</p>,
+<p>
+(3)	The Senior Treasurer shall make arrangements for the Club’s Accounts to
+be properly audited, either by themselves, or by some other person approved
+under University Ordinances;
+</p>,
+<p>
+(4)	The Senior Treasurer shall not be liable for any financial debt or other
+obligation of the Club unless personally authorised such a debt in writing;
+</p>,
+<p>
+(5)	For so long as the Club shall be Registered with the Junior Proctor, it
+shall be the duty of the Executive Committee to ensure that the Club complies
+with the requirements for Registration as a University Society.
+</p>,
+<p>
+<h3><strong>7.	CHANGES TO THE CONSTITUTION:</strong></h3>
+</p>,
+<p>
+The Constitution may be amended at a General Meeting, with approval of at
+least two thirds of those present. No amendment to this Constitution intended to
+remove the position of Senior Treasurer, to alter its prerogatives and duties,
+or to change the criteria and procedure for the appointment of a Senior
+Treasurer can be put to a vote without the prior written agreement of the Junior
+Proctor of the University of Cambridge.
+</p>,
+<p>
+<h3><strong>8.  DISCIPLINARY PROCESSES:</strong></h3>
+</p>,
+<p>
+The Club’s only disciplinary sanctions are to expel or to suspend a
+member whose actions are felt to be such as to bring the Club into disrepute, or
+to adversely affect the smooth running of the Club in a major way. Such
+expulsion or suspension can only be effected by a majority vote of all Executive
+Committee members, excluding the Senior Treasurer. In the event of an expulsion,
+the excluded member is required to return all equipment, documents and finances
+belonging to the Club within 7 days. They will not be entitled to any full or
+partial refund of annual subscriptions;
+</p>,
+<p>
+If the excluded or suspended member wishes to appeal the decision, that
+appeal should be made to the Senior Treasurer, who will consider all the facts, and whose decision will be
+final. An appeal as to the process of exclusion, but not a further investigation into the
+facts, may be lodged with the Junior Proctor.
+</p>,
+<p>
+<h3><strong>9.	DISSOLUTION:</strong></h3>
+</p>,
+<p>
+(1)	The Club may be dissolved at a General Meeting provided that at least
+Twenty-one days written notice of the intention to dissolve the Club has been
+given to the members. At least two thirds of those present and voting at the
+General Meeting must vote in favour of the motion for Dissolution for it to be
+effective;
+</p>,
+<p>
+</p>,
+<p>
+(2)	Any motion for Dissolution of the Club shall provide that assets
+remaining after all liabilities have been met shall be transferred either to
+another Registered University Club, or to the Societies’ Syndicate.
+</p>,
+<p>
+<h3><strong>10.	CLUB MEETS:</strong></h3>
+</p>,
+<p>
+</p>,
+<p>
+(1) A meet shall be defined as “an outing proposed by an Officer to a
+destination of their choosing, and of a duration of their choosing.” The Officer
+in question need not attend the meet.
+</p>,
+<p>
+(2)	The Executive Committee shall determine an appropriate fee scale for
+meets. Prices are to be determined by the Executive committee, and are to be
+reviewed at the beginning of each academic year.
+</p>,
+<p>
+(3)	Members that drive their own vehicles to support club meets are to be
+paid ‘wear and tear’. Prospective members are welcome on two trips, each with a
+maximum duration of one day.	(This needs to be updated to correspond with the
+latest BMC rules on prospective members)
+</p>,
+
+]
+
+const dataAbout = [
+<h3>
+<strong>How we use your personal information – Cambridge University
+Mountaineering Club</strong>
+</h3>,
+<br></br>,
+<p>
+This statement explains how the Cambridge University Mountaineering Club (“the
+club”, “we”, “us” and “our”) handles and uses the personal information we
+collect about our members for processes relating to our operations and
+activities
+</p>,
+<p>
+When changes are made to this statement, we will publish the updated version on
+our website.
+</p>,
+<p>
+The controller for your personal information is Cambridge University
+Mountaineering Club The person responsible for data protection within our
+society is the President who can be contacted at president@cumc.org.uk.
+</p>,
+<p>
+<strong>How we use your personal information</strong>
+</p>,
+<p>
+We collect and process your personal information for a number of purposes,
+including:
+</p>,
+<ul>
+    <li>Contacting you with news, updates, and information from the club.</li>
+    <li>Keeping a record of meet attendance.</li>
+    <li>Signing up paid members for BMC membership.</li>
+</ul>,
+<p>
+<strong>How we share your personal information</strong>
+</p>,
+<p>
+We share some of your personal information with the University, only where there
+is a specific need to, including for registration as a University Sports Club,
+and to provide your Blues and   sporting records for your University records and
+for alumni communications and/or fundraising purposes specifically related to
+the sports club.
+</p>,
+<p>
+We share some of your personal information with the British Mountaineering
+Council for the purposes of insurance and to allow them to provide services
+including member discounts and publications. The University of Cambridge
+requires this insurance to be in place in order for the club to operate.
+</p>,
+<p>
+We share some if your personal information with people attending meets to
+facilitate running the meet and for use in the event of an accident or
+emergency.
+</p>,
+<p>
+We use University and non-University IT facilities to store electronic copies of
+personal information.
+</p>,
+<p>
+We may also be subject to a legal requirement (with or without your consent) to
+share your personal information with the University or a government agency (such
+as the police or security services or other statutory authorities with
+investigatory powers) under special circumstances (e.g. relating to tax, crime
+or health and safety).  In particular we will share personal information with
+emergency services in case of an accident. Where feasible and appropriate, we
+will notify you of our intention to share such information in advance.
+</p>,
+<p>
+<strong>Your rights</strong>
+</p>,
+<p>
+You have the right to access the personal information that we hold about you.
+You also have the right to ask us to correct any inaccurate personal information
+we hold about you, to delete personal information, or otherwise restrict our
+processing, or to object to processing or communications, or to receive an
+electronic copy of the personal information you provided to us. Please note that
+all of these rights are qualified in various ways.
+</p>,
+<p>
+If you have questions or concerns about how your personal information is used,
+please contact us using the above details.
+</p>,
+<p>
+If you remain unhappy with the way your information is being handled, or with
+the response received from us, you have the right to lodge a complaint with the
+Information Commissioner’s Office at Wycliffe House, Water Lane, Wilmslow, SK9
+5AF (<a href="https://ico.org.uk/">https://ico.org.uk/</a>).
+</p>,
+<p>
+Last updated: 4 September 2020
+</p>,
+
+]
+
 const safetyAbout = [
-    <h2>Cambridge University Mountaineering Club Safety Policy</h2>,
+    <h2><strong>Cambridge University Mountaineering Club Safety Policy</strong></h2>,
     <br />,
-    <h3>1 Introduction</h3>,
+    <h3><strong>1 Introduction</strong></h3>,
     <br />,
     "This policy is designed to outline the various safety issues that have been considered by the club and how they affect its running.",
     <br />,<br />,
@@ -19,7 +334,7 @@ const safetyAbout = [
     " to fulfil effectively their health and safety responsibilities. In taking up a position of a club officer an individual must understand"+
     " that they are accepting responsibility and they must fulfil those duties to the best of their ability without negligence. ",
     <br />,
-    <h3>2 Safety on meets</h3>,
+    <h3><strong>2 Safety on meets</strong></h3>,
     <br />,
     "No member is responsible for any other member regardless of experience; each member is responsible for their own actions and decisions"+
     " whilst climbing. This is a direct reflection of the BMC participation statement: ‘The BMC recognises that climbing, hill walking and"+
@@ -46,7 +361,7 @@ const safetyAbout = [
     "A suitable number of first aid kits must be taken on all trips; ‘suitable’ equates to a minimum of one per group for single-pitch cragging"+
     " outings, and one per rope for outings in the mountains.",
     <br />,
-    <h3>2.1 Driving on meets</h3>,
+    <h3><strong>2.1 Driving on meets</strong></h3>,
     <br />,
     "Drivers use cars at their own risk, whether they own the car or have hired it. They are responsible for ensuring that they are correctly"+
     " licensed, taxed and insured and that their car is road legal.",
@@ -58,12 +373,12 @@ const safetyAbout = [
     " depreciation, tax etc. If a car is hired then the driver will simply be reimbursed for the cost of hire and fuel (receipts must be"+
     " retained as proof of purchase). Those hiring for the club should first read the guide to hiring on the club website.",
     <br />,
-    <h3>3 Insurance</h3>,
+    <h3><strong>3 Insurance</strong></h3>,
     <br />,
     "The Cambridge University mountaineering club is an affiliated club of the British Mountaineering Council (BMC). The BMC provides club"+
     " liability insurance cover for the club and its members. Cover is applicable worldwide, subject to the exclusion of any legal action brought against the insured in a court of Law within the USA/Canada. This does not mean that activities in the USA/Canada are excluded, only that legal actions brought in the USA/Canada are excluded. It is recommended that members take out their own personal accident and travel insurance for trips that take place abroad.",
     <br />,
-    <h3>4 Risk assessments</h3>,
+    <h3><strong>4 Risk assessments</strong></h3>,
     <br />,
     "It is a requirement of the University to have an up to date risk assessment for club activities. It is a condition of funding from the"+
     " Societies Syndicate that the safety procedures of such clubs (that do hazardous activities) are approved by the Director of Physical Education.",
@@ -72,12 +387,12 @@ const safetyAbout = [
     " and (c) hill walking. These risk assessments should be reviewed on an annual basis. If any individual should wish to inspect them, please"+
     " contact the Secretary.",
     <br />,
-    <h3>5 Equipment</h3>,
+    <h3><strong>5 Equipment</strong></h3>,
     <br />,
     "The Gear Secretary is responsible for the lending of club equipment to members. They will follow the British Mountaineering Council"+
     " recommendations for care and maintenance of club kit.",
     <br />,
-    <h3>5.1 Equipment lending</h3>,
+    <h3><strong>5.1 Equipment lending</strong></h3>,
     <br />,
     "The club and the Gear Secretary shall endeavour to keep the club gear in good condition, however those borrowing the gear must understand"+
     " that we are ultimately a student society with limited resources. The borrower must make their own assessment of the condition of any gear"+
@@ -103,7 +418,7 @@ const safetyAbout = [
          has corrosion which cannot easily be removed it should be retired.
         </li>
     </ul>,
-    <h3>5.2 Equipment maintenance</h3>,
+    <h3><strong>5.2 Equipment maintenance</strong></h3>,
     <br />,
     "A record of the purchase details (including date) and safety checks of club safety gear should be kept.",
     <br />,<br />,
@@ -117,7 +432,7 @@ const safetyAbout = [
     <br />,<br />,
     "All other gear can be used indefinitely, as long as it is deemed to be in reasonable condition.",
     <br />,
-    <h3>6 Lost and stolen policy</h3>,
+    <h3><strong>6 Lost and stolen policy</strong></h3>,
     <br />,
     "The club shall attempt to recoup money from those who lose or steal club equipment. Losing gear includes getting trad gear stuck, or any"+
     " situation where the borrower did not take reasonable steps to look after the gear. If in doubt, ask the question “if this person borrowed"+
@@ -135,13 +450,13 @@ const safetyAbout = [
     "This is a guideline only; it may be that the equipment has no ‘recommended maximum age’, or that other unprecedented circumstances render"+
     " the above inappropriate. In such cases, the Gear Secretary should consult the President for further advice.",
     <br />,<br />,
-    <h3>7 Emergency procedures</h3>,
+    <h3><strong>7 Emergency procedures</strong></h3>,
     <br />,<br />,
     "In the first instance club members should ensure their own safety. In the event of an accident the emergency services should be called"+
     " where necessary and the university informed. All incidents and accidents must be reported by submitting an accident report form to the"+
     " Physical Education Department.",
     <br />,
-    <h3>8 Changes to policy</h3>,
+    <h3><strong>8 Changes to policy</strong></h3>,
     <br />,
     "This document should be kept up to date; a revised version can be approved by a simple majority of votes from the committee.",
     <br />
