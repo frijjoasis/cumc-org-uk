5412743fc65d3ded123e936c471a233cc794afd5
frijjoasis <45109609+frijjoasis@users.noreply.github.com>
2020-10-07 15:45:58 +0100

    bug fix

--- client/src/dashboard/views/meets/social/text.js
+++ client/src/dashboard/views/meets/social/text.js
@@ -3,10 +3,11 @@ import React from "react";
 const socialMeets = [
     <p>
         The club has it's weekly social at a local pub, usually somewhere with a good ale selection, straight after the
-        weekly Rainbow Rocket meet on a Monday night. We head to the pub around 9pm, so listen out for the Call to Beer
+        weekly Rainbow Rocket meet on a Thursday night. We head to the pub around 9pm, so listen out for the Call to Beer
         if you're in Rainbow Rocket, or feel free to meet us at the pub if not! Look out for the weekly email which
-        will let you know which pub we'll be at. As well as this, there are frequent extra social events throughout the
-        year, such as our christmas formal, annual dinner and swaps with other societies.
+        will let you know which pub we'll be at. (Obviously, this term this will be COVID dependent!) As well as this,
+        there are frequent extra social events throughout the year, such as our christmas formal, annual dinner and
+        swaps with other societies.
     </p>
 ];
 
