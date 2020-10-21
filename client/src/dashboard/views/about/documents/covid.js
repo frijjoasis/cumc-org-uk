3f45a1e9f3333830143d40dfbc1c72acea3bbb0e
condimentman <70375074+condimentman@users.noreply.github.com>
2020-10-21 18:25:33 +0100

    covid doc
    (cherry picked from commit 45da9a7a42241e35b576eadd48368f81df8fdc55)

---
+++ client/src/dashboard/views/about/documents/covid.js
@@ -0,0 +1,209 @@
+import React from "react";
+
+const covidAbout = [
+    <h1>CUMC COVID Guidelines</h1>,
+<p>
+This document will attempt to outline the changes to each aspect of club
+activity due to COVID-19. All of the following is subject to change, as we as a
+club have to respect the guidelines set by the BMC, University and Government
+and other bodies, which are all being updated regularly. We will attempt to keep
+this document as up to date as possible with the latest from us in terms of
+planned activities and restrictions.
+</p>,
+<h2><strong>Outdoor Meets</strong></h2>,
+<p>
+Outdoor meets will currently be taking part this term unless there is any
+significant change in the coming weeks.
+</p>,
+<p>
+There will be a number of measures in place to make meets Covid safe and we will
+be adhering to all the BMC guidelines given to student clubs.
+</p>,
+<p>
+<strong>The measures that we will be adhering to on meets are:</strong>
+</p>,
+<ul>
+    <li>Students must not attend meets if they have any symptoms of Covid-19</li>
+    <li>Providing they are able to, students must be taking part in the University
+    testing service to attend meets</li>
+    <li>If they are able to take part in the above and providing it is running
+    effectively, students must have a recent (circa 2 weeks) negative test in order
+    to attend meets</li>
+    <li>Students must not attend if anyone in their household is displaying symptoms</li>
+    <li>Students must adhere to all college and government regulations in order to
+    attend meets</li>
+    <li>Food and drink sharing will <strong>not</strong> be permitted and students
+    must bring sufficient food and water for themselves</li>
+    <li>In line with government guidelines we are allowed to run meets up to a size
+    of 30</li>
+    <li>No overnight meets will be taking place this term and we will review this
+    with the situation in January</li>
+    <li>Contact tracing details of students will be taken for each meet by the
+    organiser</li>
+    <li>Students are advised to bring their own personal first aid kits if they have
+    them</li>
+    <li>A covid risk assessment form is to be completed for each meet based on the
+    BMC template</li>
+</ul>,
+<p>
+<strong>Regarding transport:</strong>
+</p>,
+<ul>
+    <li>Masks are to be worn in the car at <strong>all times</strong> (except by
+    those exempt)<strong> </strong>and students must use hand sanitizer upon
+    entering and exiting the car</li>
+    <li>Windows of the car are to be kept down by at least ⅓ for the duration of the
+    journey and students are advised to travel in warm layers that you should have
+    for the crag anyway</li>
+    <li>Fuel stops are allowed and only the driver may enter the shop to pay, if
+    necessary other students may visit the toilet</li>
+    <li>No other stops are allowed on the journey</li>
+</ul>,
+<p>
+<strong>Regarding climbing itself</strong>
+</p>,
+<ul>
+<li>Students should maintain 2m social distancing at all times, with the
+exception of when passing a belayer at the top of the route where students
+should attempt to turn their faces away from each other when they pass</li>
+<li>Use hand sanitiser between climbs</li>
+<li>The venue will be carefully assessed by the outdoor meet secretaries in
+advance of the meet to try and ensure that quiet and appropriate locations are
+chosen</li>
+<li>Unless it will compromise safety whilst leading a route students should not
+be placing gear or rope in their mouths whilst climbing</li>
+</ul>,
+<p>
+<strong>Indoor meets</strong>
+</p>,
+<ul>
+    <li>General guidance: see outdoor meets for general CUMC meets policies</li>
+    <li>Indoor meets will run weekly, day TBC (we are currently communicating with
+    the gyms we usually visit to see what works best)</li>
+    <li><strong>In the gym</strong>: All gyms will have their own policies so
+    essentially we do what the gym tells us to do.</li>
+    <li><strong>Numbers</strong>: Either 6 max or groups of 6. This will be based on
+    what version of the rule of 6 gyms allow</li>
+    <li><strong>How we’ll run things: </strong>Signup on the website as with all
+    CUMC meets. We will organise the thing but it will just be a case of
+    week-to-week organisation in keeping with guidance. We’re trying to keep things
+    as flexible as possible so that we can run as much as possible rather than just
+    blanket prohibiting anything. We’ll essentially allow what the government and
+    BMC allow, so everything is <strong>subject to change</strong>, but we’ll keep
+    you updated</li>
+    <li><strong>Transport</strong>: We both have a car (the indoor secs), so between
+    us we can take up to 6 people. However we won’t always be able to go and
+    generally it might be that only one of us can attend. Therefore we would like
+    participants to think about whether they would still want to attend if they were
+    required to take public transport (this should be included as an option on the
+    signup form), since there are gyms accessible by train from Cambridge and if we
+    can’t find a driver this will be our best option.</li>
+</ul>,
+<h2><strong>Competitions</strong></h2>,
+<p>
+<strong>There has been no information to suggest that any of our major
+competitions will be cancelled.</strong> However, with the IFSC cancelling World
+Cups for the rest of the year, there is a chance that Michaelmas Term
+competitions (i.e. the first LUBE) will be cancelled.
+</p>,
+<p>
+<strong>Transport </strong>can be discussed with competitors who choose to
+attend closer to the time based on safety and government regulations, but will
+likely involve trains and not car sharing.
+</p>,
+<p>
+<strong>BUCS</strong>: Currently scheduled to go ahead (Lent Term).
+</p>,
+<p>
+<strong>LUBE</strong>: To go ahead, with some changes. Teams will have a 2-3
+week window to go down and try the competition blocks once. 4 qualification
+rounds as usual, and hopefully a final at the end of the year. Scored on
+attempts, as usual, to keep things fair for us and other non-London unis.
+</p>,
+<p>
+<strong>Varsity</strong>: Usually in Lent Term, so hopefully/likely to go ahead.
+Oxford to organise this year?
+</p>,
+<p>
+<strong>Cuppers</strong>: To go ahead, provided RR is allowed to stay open under
+government guidance. All scoring can be done digitally (no printed sheets to be
+provided). People can partake in their own time at safe distances from other
+climbers. If a second lockdown occurs, there is a possibility of another virtual
+Cuppers.
+</p>,
+<p>
+<strong>Training</strong>: I am currently re-writing the training regime. Old
+plans focused on group workouts in the relatively small upstairs training room
+at RR. New plans will be more climbing specific and allow spacing around the
+centre. Digital notes will be provided so that people who wish to avoid larger
+CUMC groups can undertake a weekly training session in their own time.
+</p>,
+<p>
+I (Hugo) am currently shielding and would prefer not to return to main CUMC
+sessions for at least a while. If anyone else on the committee wants to run
+training sessions for those who don’t mind attending at popular times, please
+get in contact with me!
+</p>,
+<h2><strong>Socials</strong></h2>,
+<p>
+<strong>Weekly Socials:</strong>
+</p>,
+<ul>
+    <li>Socials will be happening on Thursday afternoons, as Rainbow Rocket has
+    introduced stricter policies on customer numbers</li>
+    <li>After-climbing socials will be outdoors in gro</li>ups of up to 6. Earlier than
+    usual (afternoon/early evening) to avoid any issues with curfews
+    <li>There is the possibility of moving to pubs later in term if it’s reasonable
+    to do so (depending on household mixing laws)</li>
+</ul>,
+<p>
+<strong>Other</strong>
+</p>,
+<ul>
+    <li>Online socials may occur, subject to demand - outdoor socials will almost
+    certainly prove to be more popular than online</li>
+    <li>Small swaps (Gardies are allowing swaps of 2 groups of 6) would depend on
+    household mixing laws, and the situation with COVID. This will be reviewed later
+    in term.</li>
+    <li>If the guidance changes back to allowed groups of 30+ (or thereabouts), we
+    could do outdoor socials in larger groups. This will remain under review.</li>
+</ul>,
+<h2>Gear</h2>,
+<p>
+<strong>RESTRICTED GEAR LENDING </strong>
+</p>,
+<p>
+<strong>Picking Up Gear</strong>
+</p>,
+<ul>
+    <li>Anyone wishing to pick up gear must message Gear Secretary at least the day
+    before with the gear they want and the time they wish to pick it up</li>
+    <li>Everyone must wash hands before picking up gear</li>
+    <li>Everyone must wear a face mask when picking up gear</li>
+    <li>Gear will either be picked up from an outside location - agreed by Gear
+    Secretary and club member</li>
+    <li>Gear Secretary must clean gear according to manufacturer’s instructions</li>
+    <li>Gear Secretary must place gear down and retreat a safe distance</li>
+    <li>Gear Secretary must record the gear and the time it was taken
+</li>
+</ul>,
+<p>
+<strong>Dropping Off Gear</strong>
+</p>,
+<ul>
+    <li>Anyone wishing to drop off gear must message Gear Secretary at least the day
+    before with the gear they have and the time they wish to drop it off</li>
+    <li>Everyone must wash hands before dropping off gear</li>
+    <li>Everyone must wear a face mask when dropping off gear</li>
+    <li>Gear will either be dropped off from an outside location - agreed by Gear
+    Secretary and club member</li>
+    <li>Club member must place gear down and retreat a safe distance</li>
+    <li>Gear Secretary must collect gear wearing disposable gloves</li>
+    <li>Gear Secretary must bag gear and quarantine in room for at least 72 hours</li>
+    <li>After 72 hours Gear Secretary must clean gear according to manufacturer's
+    instructions
+    </li>
+</ul>
+];
+
+export {covidAbout}
