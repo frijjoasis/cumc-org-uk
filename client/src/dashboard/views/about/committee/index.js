9d9a329503c5654ce3af2bbdf15272f39227739b
condimentman <70375074+condimentman@users.noreply.github.com>
2020-10-21 23:11:54 +0100

    Old Committee

--- client/src/dashboard/views/about/committee/index.js
+++ client/src/dashboard/views/about/committee/index.js
@@ -11,13 +11,25 @@ import Table from "react-bootstrap/Table";
 
 import {committee} from "./committee";
 
+const oldCommittee = {
+    'head' : ["Year","President","Secretary","Treasurer","Gear","Outdoor Meet","Indoor Meet","Alpine and Winter","Competition","Social","Journal","Librarian","Webmaster"],
+    'body' : [
+                ["2018-2019","Holly Rowland","Izzy Bentley","Sophie Miocevich","Elin Falla","Omar Shah & Timo Zheng","Alex Nicol & Bethan Morris","Charlie Fraser","Ed Mabon","Joe Taylor & Sophia Georgescu","Sophie Miocevich","Francesca Rigg","Daniel Wainwright"],
+                ["2017-2018","Ed Wheatcroft","Jemima Churchhouse","Gwilym Rowbottom","Sophie Miocevich","Ed Bray & Robert Morse","Holly Rowland & Alice Kirk","James Kent","Timo Zheng","Isabella Bentley & Joe Taylor","Jia Yuan Loke & Charlie Fraser","Jia Yuan Loke & Charlie Fraser","James Lomax"],
+                ["2016-2017","Gwilym Rowbottom","Kris Cao","Liat Adler","George Harding-Perrott","Ed Wheatcroft & Alex Law","Ed Bray","Cameron Holloway","Isabella Bentley","Roberto Cecere & Jemima Churchhouse","none","Gwilym Rowbottom",""],
+                ["2015-2016","Philip Glass","Daniel Zheng","Matthew Wong","Veronika Siska","Ed Wheatcroft & Andrew Cherenkov","Kris Cao","Will Kernick","Gwilym Rowbottom","Ed Bray","Daniel Zheng","Rose Pearson",""],
+                ["2014-2015","Tom Hare","Laurent Michaux","Luke Bounds","Daniel Zheng","Alexander Law","Helen Fox & Liam Carter","Rose Pearson","Philip Glass","Lucy Sawyer","Cameron Holloway","Evan Miles",""],
+                ["2013-2014","Liam Carter","Laurence Cowton","Luke Bounds","Philip Glass","Daniel Zheng and Thomas Geh","Daniel Zheng and Thomas Geh","Tom Hare","Amelia Fischer-Linnett","Nick Jarman and Laurence Orchard","Jack Ogden and Nick Jarman","Evan Miles",""],
+                ["2012-2013","Laurence Cowton","Fabian Jakubczik","Ed Feldman","Vincent Lister","Liam Carter","Liam Carter","Tom Hare (Alpine) & Ivo Dawkins (Winter)","Amelia Fischer-Linnett","Jack Ogden","Nick Jarman","Dawn Hollis",""]
+            ]}
+
 class CommitteeAbout extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             content: {
-                head: [],
-                body: [],
+                head: oldCommittee.head,
+                body: oldCommittee.body,
             }
         }
     }
@@ -96,7 +108,8 @@ class CommitteeAbout extends React.Component {
                                         Here you can find the committees of years past.
                                     </Card.Subtitle>
                                     <hr />
-                                    <Table striped bordered hover responsive>
+                                    <Table striped bordered hover>
+                                        <div  style={{fontSize: 9}}>{/*can't get responsive table to work inside card*/}
                                         <thead>
                                         <tr>
                                             {this.state.content.head.map((prop, key) => {
@@ -105,16 +118,19 @@ class CommitteeAbout extends React.Component {
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        {this.state.content.body.map((prop, key) => {
-                                            return (
-                                                <tr key={key}>
-                                                    {prop.map((prop, key) => {
-                                                        return <td key={key}>{prop}</td>;
-                                                    })}
-                                                </tr>
-                                            );
-                                        })}
+                                            
+                                            {this.state.content.body.map((prop, key) => {
+                                                return (
+                                                    <tr key={key}>
+                                                        {prop.map((prop, key) => {
+                                                            return <td key={key}>{prop}</td>;
+                                                        })}
+                                                    </tr>
+                                                );
+                                            })}
+                                    
                                         </tbody>
+                                        </div>
                                     </Table>
                                 </Card.Body>
                             </Card>
