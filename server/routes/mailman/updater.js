1f6e11a69c68038748d3f28617b2dea01a512885
Edmund Ross <45109609+frijjoasis@users.noreply.github.com>
2021-10-05 18:48:06 +0100

    oops

--- server/routes/mailman/updater.js
+++ server/routes/mailman/updater.js
@@ -36,7 +36,7 @@ fs.readFile('/societies/cumc/cumc-org-uk/server/routes/mailman/mail.json', (err,
 
         // Reset mail.json. We don't have to worry about this being asynchronous from the child processes, the file has already been read
         fs.writeFile('/societies/cumc/cumc-org-uk/server/routes/mailman/mail.json', empty, 'utf-8', err => {
-            if (!err) logger.log("Reset mail.json successfully.");
+            if (!err) logger.info("Reset mail.json successfully.");
             else logger.error("Failed to write to mail.json: ", err);
         });
     } else logger.error("Failed to read mail.json: ", err);
