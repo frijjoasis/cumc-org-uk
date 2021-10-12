8214721034e4208bf5ac2058fb0b4ce2ad6409c6
Edmund Ross <45109609+frijjoasis@users.noreply.github.com>
2021-10-12 10:51:41 +0100

    hotfix
    hotfix
    
    Revert "logger"
    
    This reverts commit a1d06d6930c929739212211c1f46ae6f7940453a.

--- server/routes/mailman/updater.js
+++ server/routes/mailman/updater.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 // Run as a cron job every couple of hours
 const fs = require('fs');
-const logger = require('../../logger').logger;
 const spawn = require('child_process').spawn;
 
 const empty = JSON.stringify({
@@ -17,14 +16,14 @@ const empty = JSON.stringify({
     }
 }, null, 2);
 
-fs.readFile('mail.json', (err, data) => {
+fs.readFile('/societies/cumc/cumc-org-uk/server/routes/mailman/mail.json', (err, data) => {
     if (!err) {
         try {
             const lists = JSON.parse(data.toString());
 
             ["add", "remove"].forEach(i => {
                 Object.keys(lists[i]).forEach(list => {
-                    let child = spawn(`srcf-mailman-${i}`, (i === "add") ? ['-w', 'y', '-a', 'n', list]
+                    let child = spawn(`/usr/local/bin/srcf-mailman-${i}`, (i === "add") ? ['-w', 'y', '-a', 'n', list]
                         : ['-N', '-s', list]);
                     child.stdin.setEncoding('utf-8');
                     for (let e of lists[i][list]) {
@@ -35,15 +34,13 @@ fs.readFile('mail.json', (err, data) => {
                 });
             });
         } catch(err) {
-            logger.error("Failed to update mail preferences: ", err);
+            console.error("Failed to update mail preferences: ", err);
         }
 
         // Reset mail.json. We don't have to worry about this being asynchronous from the child processes, the file has already been read
         fs.writeFile('mail.json', empty, 'utf-8', err => {
             if (!err) console.log("Reset mail.json successfully.");
-            else logger.error("Failed to write to mail.json: ", err);
+            else console.error("Failed to write to mail.json: ", err);
         });
-    } else logger.error("Failed to read mail.json: ", err);
+    } else console.error("Failed to read mail.json: ", err);
 });
-
-// Log errors with winston, otherwise log to the console (and thus email, since cron job)
