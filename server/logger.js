7a296f9f50ec0addb26e68a1866ef587dbf4f298
frijjoasis <45109609+frijjoasis@users.noreply.github.com>
2022-10-15 02:14:03 +0100

    try this

--- server/logger.js
+++ server/logger.js
@@ -1,46 +1,58 @@
+const fs = require('fs')
 const winston = require('winston');
 const expressWinston = require('express-winston');
 require('winston-daily-rotate-file');
+
+format = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.json()
+)
 
 function transport(path, level) {
     return new winston.transports.DailyRotateFile({
         filename: `/societies/cumc/cumc-org-uk/logs/${path}`,
         options: {mode: 0o660}, // File permissions
-        datePattern: 'YYYY-MM-DD_HH:mm:ss',
+        datePattern: 'YYYY-MM-DD',
         auditFile: '/societies/cumc/cumc-org-uk/logs/audit.json',
         level: level ? level : 'info',
+        handleExceptions: true,
+        handleRejections: true,
         maxSize: '50m',
         maxFiles: '14d'
     });
 }
 
+transports = [transport('access-%DATE%.log'),
+    transport('application-%DATE%.log'),
+    transport('exceptions-%DATE%.log', 'error')
+]
+
 let expressLogger = expressWinston.logger({
     transports: [
-        transport('access-%DATE%.log')
+        transports[0]
     ],
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.json()
-    ),
+    format: format,
     msg: "HTTP {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
 });
 
 const logger = winston.createLogger({
     transports: [
-        transport('application-%DATE%.log'),
-        transport('exceptions-%DATE%.log', 'error')
+        transports[1],
+        transports[2]
     ],
-    exceptionHandlers: [
-        transport('application-%DATE%.log'),
-        transport('exceptions-%DATE%.log')
-        // Log errors to both files
-    ],
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.json()
-    )
+    format: format
+});
+
+transports.forEach(t => {
+    t.on('new', function (newFilename) {
+        let data = fs.readFileSync(newFilename);
+        let fd = fs.openSync(newFilename, 'w+');
+        let buffer = Buffer.from('_____PROCESS RESTART_____\n');
+        fs.writeSync(fd, buffer, 0, buffer.length, 0); // Write new data
+        fs.writeSync(fd, data, 0, data.length, buffer.length); // Append old data
+        fs.close(fd);
+    });
 });
 
 module.exports = {
