b369ee5da27da5925a57d00b7cad26739a7fc3c6
Jake Thakur <34597857+jakethakur@users.noreply.github.com>
2023-09-04 12:07:44 +0100

    fixed broken link

--- client/src/dashboard/views/meets/social/text.js
+++ client/src/dashboard/views/meets/social/text.js
@@ -5,7 +5,7 @@ const socialMeets = [
         The club has its weekly social at a local pub, straight after the
         weekly Kelsey Kerridge meet on a Monday night. We head to the pub around 9pm, so listen out for the Call to Beer
         if you're at the wall, or feel free to meet us at the pub if not! Keep an eye on the
-		<a href={link}> Whatsapp chat <i className="fa fa-whatsapp"/></a>to see
+		 Whatsapp chat to see
 		which pub we'll be heading to. As well as this,
         there are weekly social events throughout the year, such as movie nights, our annual dinner and
         swaps with other societies. You can find this term's termcard on our
