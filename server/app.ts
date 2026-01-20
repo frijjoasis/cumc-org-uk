37398b6bbc1d4d9f3810af885a8b084c642e64b2
Ben Browning <bbrowning040518@gmail.com>
2026-01-20 04:35:02 +0000

    small fix?

--- server/app.ts
+++ server/app.ts
@@ -27,7 +27,7 @@ import mailmanRouter from './routes/mailman/mailman.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.join(__dirname, '.env') });
+dotenv.config({ path: path.resolve(process.cwd(), 'server/.env') });
 
 const app = express();
 const port = process.env.PORT || 5000;
